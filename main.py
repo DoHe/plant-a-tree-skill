095e71e34ebc37f8ed25cc67b22b42e0d68e1c53
Dominik <dominik.henter@ecosia.org>
2018-07-06 15:07:18 +0200

    Smarter responses

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import json
 class SkillHandler(BaseHTTPRequestHandler):
     
     def do_POST(self):
+        speech = not_understood()
         content_length = int(self.headers.get('content-length', 0))
         if content_length:
-            body = self.rfile.read(content_length)
-            parsed = json.loads(body)
-            print(parsed)
+            try:
+                body = self.rfile.read(content_length)
+                parsed = json.loads(body)
+                if parsed['request']['intent']['name'] == 'plant_tree':
+                    speech = plant_trees(parsed['request']['intent']['slots']['number']['value'])
+            except Exception as e:
+                print(e)
+        
         response = {
             "version": "1.0.0",
             "response": {
-                "outputSpeech": {
-                    "type": "PlainText",
-                    "text": "Your trees have been planted. Thank you for saving the world!",
-                    "ssml": "<speak><p>Your trees have been planted.</p><p>Thank you for saving the world!</p></speak>"
-                },
+                "outputSpeech": speech,
                 "shouldEndSession": True
             }
 
@@ -24,7 +26,34 @@ class SkillHandler(BaseHTTPRequestHandler):
         self.send_response(200, "All OK")
         self.end_headers()
         self.wfile.write(json.dumps(response).encode())
-        return
+
+
+def not_understood():
+    return {
+        "type": "PlainText",
+        "text": "I heard something about trees, that's awesome. But I didn't fully get you, can you repeat please?",
+        "ssml": "<speak><p>I heard something about trees, that's awesome.</p><p>But I didn't fully get you, can you repeat please?</p></speak>"
+    }
+
+
+def plant_trees(number):
+    if number is None:
+        return {
+            "type": "PlainText",
+            "text": "I'll just do ahead and assume you want to plant just a single tree (which I now did). Thank you for saving the world!",
+            "ssml": "<speak><p>I'll just do ahead and assume you want to plant just a single tree (which I now did).</p><p>Thank you for saving the world!</p></speak>"
+        }
+    if number <= 0:
+        return {
+            "type": "PlainText",
+            "text": "You really have to plant more than 0 tress to save the world...",
+            "ssml": "<speak>You really have to plant more than 0 tress to save the world...</speak>"
+        }
+    return {
+        "type": "PlainText",
+        "text": f"You just planted {number} trees. You rock!",
+        "ssml": f"<speak><p>You just planted {number} trees.</p><p>You rock!</p></speak>"
+    }
 
 
 def main(port):
@@ -32,6 +61,7 @@ def main(port):
     httpd = HTTPServer(server_address, SkillHandler)
     print(f"Listening on port {port}...")
     httpd.serve_forever()
+
 
 if __name__ == '__main__':
     import sys
