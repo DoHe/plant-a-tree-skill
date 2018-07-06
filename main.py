3831063bd8c5439a1fc45d43ceecb3f7b958c96e
Dominik <dominik.henter@ecosia.org>
2018-07-06 13:24:00 +0200

    Simple server

---
+++ main.py
@@ -0,0 +1,22 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
+class SkillHandler(BaseHTTPRequestHandler):
+    
+    def do_POST(self):
+        content_length = int(self.headers.get('content-length', 0))
+        if content_length:
+            body = self.rfile.read(content_length)
+            parsed = json.loads(body)
+            print(parsed)
+        self.send_response(200, "All OK")
+        self.end_headers()
+
+def main(port):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, SkillHandler)
+    print(f"Listening on port {port}...")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    main(8765)
